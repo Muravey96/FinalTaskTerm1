f02acc278882d68ddcf20b9ff5b050533c98a165
DariaMuravitskaya <muraviczkaya96@gmail.com>
2022-12-23 19:50:15 +0300

    added main block

--- FinalTask/FinalTaskProg.cs
+++ FinalTask/FinalTaskProg.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Console.Write("Введите целое число - Сколько элементов необходимо проверить; нажмите enter: ");
+int userNum = int.Parse(Console.ReadLine() ?? "");
+string[] userArray = FillArray(userNum);
+string[] finalArray = RemoveUnsuitElem(userArray, 3); 
+Console.WriteLine($"{String.Join(" ", userArray)}  -> {String.Join(" ", finalArray)}");
+
+////////////////////////
+
