3746d2de2be8fa6fc4e6ee4f68a2684f99e9c960
DariaMuravitskaya <muraviczkaya96@gmail.com>
2022-12-23 19:53:37 +0300

    added method RemoveUnsuitElem

--- FinalTask/FinalTaskProg.cs
+++ FinalTask/FinalTaskProg.cs
@@ -15,3 +15,22 @@ string[] FillArray(int n)
     for (int i = 0; i < n; i++) filledArray[i] = Console.ReadLine() ?? "";    
     return filledArray;
 }
+
+////////////////////////
+
+string[] RemoveUnsuitElem(string[] newArray, int num)
+{
+    int count = 0; 
+    int[] with_restrictions = new int[newArray.Length];
+    for (int i = 0; i < newArray.Length; i++)
+    {
+        if (newArray[i].Length <= num) 
+        {
+            with_restrictions[count] = i;
+            count++;
+        }
+    }
+    string[] resultArray = new string[count];
+    for (int j = 0; j < count; j++) resultArray[j] = newArray[with_restrictions[j]];
+    return resultArray;
+}
